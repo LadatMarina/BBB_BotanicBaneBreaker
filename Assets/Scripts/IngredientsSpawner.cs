a69f175b4ce192dc751fa6d07c499f4313579969
MASEPA12 <ladatmarina@gmail.com>
2024-01-23 13:10:10 +0100

    ,

--- Assets/Scripts/IngredientsSpawner.cs
+++ Assets/Scripts/IngredientsSpawner.cs
@@ -7,7 +7,12 @@ public class IngredientsSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < 3; i++) { 
+
+        GameObject fruit = new GameObject();
+            fruit.AddComponent<RecollectableDisplay>();
+            
+        }
     }
 
     // Update is called once per frame
